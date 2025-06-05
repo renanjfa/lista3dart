0e05053c9a478cb8a0c0882acb601994f393be53
renanjfa <renanjusan@gmail.com>
2025-06-05 16:01:42 -0300

    Commit final exericio 3

---
+++ exe3.dart
@@ -0,0 +1,31 @@
+import 'dart:io';
+
+void inserirLista(var l, String item) {
+  l.add(item);
+}
+
+void salvarArquivo(File arq, String l) {
+  arq.writeAsStringSync(l);
+}
+
+
+void main() {
+  var compras = [];
+  String item;
+
+  while(true) {
+    stdout.write("Digite um item da lista de compras (ou 'fim' para terminar): ");
+    item = stdin.readLineSync() ?? '0';
+    if(item == "fim") break;
+    inserirLista(compras, item);
+  }
+
+  var sink = File('minhas_compras.txt').openWrite();
+
+  for (var c in compras) {
+    sink.writeln(c);
+  }
+
+  sink.close(); 
+  print("Lista de compras salva em 'minhas_compras.txt' com sucesso!");
+}
